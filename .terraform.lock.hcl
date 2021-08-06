4ec85fbf68c6e1dadcb6ea904e25c16d06754332
Florian Röser <florian.roeser@ic-consult.com>
2021-08-06 11:55:02 +0200

    Created first playlist

---
+++ .terraform.lock.hcl
@@ -0,0 +1,23 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/conradludgate/spotify" {
+  version     = "0.2.1"
+  constraints = "~> 0.2.1"
+  hashes = [
+    "h1:W8Buq/hnhMEwUU6zKiJhNXq7/23bC5q/cBkrnahNgGg=",
+    "zh:2d7bc90c8b1fa8173dee6f888d816ae9630a8c713c9717967c36c943a60af7f0",
+    "zh:2eb962ff39645138bcbc41ae7b1adb530a60fca2cd9a9f30dabe0e1e1c46c85c",
+    "zh:4a659a38adaf2664bcb852b4d9e8e2fce9db83b132fc7a4509b7a38b5f7a3abb",
+    "zh:58ff069b0f13873a364d37467bfdb5b5b00b10bf2439030e6cc3f284808d8d50",
+    "zh:5bd3a74b8c0b812f8a05c827d85ef472bd89954e961ca62873897aaedc7a4955",
+    "zh:5c3e228783153978f77c83023d3763f5954455787a6e1b913a834e17892993bc",
+    "zh:5e197b7355430f91be36b20c0a4bf020a9f17487a2886e0bd0f38a2430fcdd58",
+    "zh:6c6966d970437ab2e8cd7a89bd2c6f3062d62c776541395ede7ebd021213615a",
+    "zh:97a6f2d848ea5e137c873585dd50d07011b2c142c1d5a5e1290f38a2428d317b",
+    "zh:9cc15acd8706b441103af49730ad1d33920218da0eefb2c42eb5c8da9abc5d33",
+    "zh:b55d18cd5b9ddd7fbd8392fb69c9f02a7f80725be06c87eefee259019279ce02",
+    "zh:b914c6f66894f3f7560d76c0bd9b1738e98762daf4b7e7e1edf4f08c31cc9884",
+    "zh:ded375a7e84bb64e6cf624d9ebf67acddc68a33708bb251bbbf3378f42d3c610",
+  ]
+}
--- main.tf
+++ main.tf
@@ -2,37 +2,69 @@ terraform {
   required_providers {
     spotify = {
       version = "~> 0.2.1"
-      source  = "conradludgate/spotify"
+      source = "conradludgate/spotify"
     }
   }
 }
 
 variable "spotify_api_key" {
   type = string
+  description = "Set this APIKey that the authorization-server outputs in the 'terraform.tfvars' file"
 }
 
 provider "spotify" {
   api_key = var.spotify_api_key
+  # Public Proxy Configuration:
+  # For a simple way to manage your spotify oauth2 tokens is to use https://oauth2.conrad.cafe.
+  #auth_server = "https://oauth2.conrad.cafe"
+  #username = "your username"
+  #token_id = "your token id"
 }
 
 resource "spotify_playlist" "playlist" {
-  name        = "Terraform Summer Playlist"
-  description = "This playlist was created by Terraform"
-  public      = true
+  name = "Terraform Summer Playlist"
+  description = "My playlist created by Terraform"
+  public = true
 
-  tracks = [
-    data.spotify_search_track.by_artist.tracks[0].id,
-    data.spotify_search_track.by_artist.tracks[1].id,
-    data.spotify_search_track.by_artist.tracks[2].id,
-  ]
+  tracks = flatten([
+    data.spotify_search_track.rem.tracks[0].id,
+    data.spotify_search_track.rem.tracks[1].id,
+    data.spotify_search_track.rem.tracks[2].id,
+    data.spotify_track.kunstfreiheit.id,
+    data.spotify_track.eine_gute_nachricht.id,
+    data.spotify_search_track.marc_cohn.tracks[0].id,
+    data.spotify_search_track.marc_cohn.tracks[1].id,
+    data.spotify_search_track.marc_cohn.tracks[2].id,
+    data.spotify_search_track.marc_cohn.tracks[3].id,
+    data.spotify_search_track.first_aid_kit.tracks[0].id,
+    data.spotify_search_track.first_aid_kit.tracks[1].id,
+    data.spotify_search_track.first_aid_kit.tracks[2].id,
+  ])
 }
 
-data "spotify_search_track" "by_artist" {
-  artists = ["Dolly Parton"]
+data "spotify_search_track" "rem" {
+  artist = "R.E.M."
   #  album = "Jolene"
   #  name  = "Early Morning Breeze"
 }
 
+data "spotify_track" "kunstfreiheit" {
+  spotify_id = "4ioQKdmkdBTkqjhfHeu4YU"
+}
+
+data "spotify_track" "eine_gute_nachricht" {
+  spotify_id = "2WhSeEawdrxjXelXpWdpzr"
+}
+
+data "spotify_search_track" "marc_cohn" {
+  artist = "Marc Cohn"
+  album = "Marc Cohn"
+}
+
+data "spotify_search_track" "first_aid_kit" {
+  artist = "First Aid Kit"
+}
+
 output "tracks" {
-  value = data.spotify_search_track.by_artist.tracks
+  value = data.spotify_search_track.rem.tracks
 }
